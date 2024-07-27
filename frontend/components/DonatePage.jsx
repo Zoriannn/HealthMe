aa05a7cc92bce5d20325ccd89117566ae1762229
Zoriannn <jzchoo2004@gmail.com>
2024-07-27 20:33:54 +0800

    added chatbot in the donation page

--- frontend/components/DonatePage.jsx
+++ frontend/components/DonatePage.jsx
@@ -1,24 +1,14 @@
-/* eslint-disable jsx-a11y/mouse-events-have-key-events */
-/* eslint-disable camelcase */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable jsx-quotes */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useRef, useState, useEffect } from 'react';
-
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { Carousel } from 'react-responsive-carousel';
-import { useDispatch, useSelector } from 'react-redux';
-import cloneDeep from 'lodash/cloneDeep';
-// import Link from 'next/link';
-import _ from 'lodash';
+import { useDispatch } from 'react-redux';
 import Aos from 'aos';
-import { ArrowRightOutlined } from '@ant-design/icons';
 import Layout from './general/Layout';
 import { SettingActions } from './reducers/settingReducer';
-import { logoIcon } from '../images';
-import { routes } from '../route';
+import Chatbot from "react-chatbot-kit";
+import config from "./chatbot/config";
+import MessageParser from "./chatbot/MessageParser";
+import ActionProvider from "./chatbot/ActionProvider";
+
 function DonatePage({ data }) {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -30,47 +20,18 @@ function DonatePage({ data }) {
 
   return (
     <Layout>
-
-      <div className='text-xl flex flex-col justify-center align-center items-center my-24'>
-
-        <div className='w-2/5'>
-          <img src="./images/logo.png" alt="logo" />
-    
+      <div className='flex flex-row h-screen'>
+        <div className='w-1/2 p-4'>
+          <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+          />
         </div>
-
-        <div className='ml-5 text-xl font-bold'>
-          {'      '}
-
-          Practise with AI Guidance.
-          {' '}
-          {' '}
-          Enhance Your Investment Confidence.
+        <div className='w-1/2 p-4'>
+          {/* Future content for the right side */}
         </div>
-
-        <div className='mt-20'>
-
-          <div
-            className='bg-red-500 rounded-full p-5 px-10 font-bold text-white cursor-pointer text-2xl'
-            onClick={() => {
-              router.push('/personality');
-              dispatch(SettingActions.setSelected(3));
-            }}
-          >
-            Getting Started
-
-            →
-
-          </div>
-
-        </div>
-        <div className='items-center align-center mt-5 font-bold justify-center'>
-          Build Your Personality Profile First!
-          {' '}
-
-        </div>
-
       </div>
-
     </Layout>
   );
 }
--- frontend/pages/donate.jsx
+++ frontend/pages/donate.jsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import _ from 'lodash';
 
-import HomePage from '../components/HomePage';
+import DonatePage from '../components/DonatePage';
 
 function Home() {
   return (
-    <HomePage />
+    <DonatePage />
   );
 }
 
